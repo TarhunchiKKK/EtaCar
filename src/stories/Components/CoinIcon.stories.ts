d1887ed00ff686a7a90b2dd93e802353b4ad528f
Kostya Barilo <kostabarilo12@gmail.com>
2024-06-10 21:23:02 +0300

    Add stories for modal widget

--- src/stories/Components/CoinIcon.stories.ts
+++ src/stories/Components/CoinIcon.stories.ts
@@ -1,15 +1,16 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { CoinIcon } from '../../shared';
+import { MockCoin } from '../mocks';
 
 const meta: Meta<typeof CoinIcon> = {
     component: CoinIcon,
-}
+};
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
 export const Simple: Story = {
     args: {
-        rank: '1'
-    }
-}
+        rank: MockCoin.rank,
+    },
+};
---
+++ src/stories/Modal/CoinsListItem.stories.ts
@@ -0,0 +1,16 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { CoinsListItem } from '../../widgets/Modal/components/CoinsListModal/CoinsListItem';
+import { MockCoin } from '../mocks';
+
+const meta: Meta<typeof CoinsListItem> = {
+    component: CoinsListItem,
+};
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Simpe: Story = {
+    args: {
+        coin: { ...MockCoin, count: 5 },
+    },
+};
